9c73e664322b186f1122e509cc0320e4198962eb
gqueste <kakashi1hatake>
2016-01-29 14:50:34 +0100

    BookActivity + amazon (Because yes)

--- app/src/main/java/com/example/gqueste/gqueste/BookActivity.java
+++ app/src/main/java/com/example/gqueste/gqueste/BookActivity.java
@@ -1,9 +1,16 @@
 package com.example.gqueste.gqueste;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.squareup.picasso.Picasso;
 
 /**
  * Created by Gabriel QUESTE on 27/01/2016.
@@ -11,6 +18,7 @@ import android.widget.Toast;
 public class BookActivity extends AppCompatActivity {
 
     public static final String BOOK = "BOOK";
+    public static final String AMAZON_ENDPOINT = "http://www.amazon.fr/s/field-keywords=";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +28,33 @@ public class BookActivity extends AppCompatActivity {
         Intent intent = getIntent();
         Book book = intent.getParcelableExtra(BOOK);
 
-        Toast.makeText(this, book.title, Toast.LENGTH_SHORT).show();
+        TextView titleTextView = (TextView) findViewById(R.id.bookTitleTextView);
+        ImageView coverImageView = (ImageView) findViewById(R.id.bookCoverImageView);
+        TextView isbnTextView = (TextView) findViewById(R.id.bookIsbnTextView);
+        TextView priceTextView = (TextView) findViewById(R.id.bookPriceTextView);
+        Button buyButton = (Button) findViewById(R.id.button_buy_amazon);
 
+        titleTextView.setText(book.title);
+        Picasso.with(this)
+                .load(book.cover)
+                .fit()
+                .centerCrop()
+                .into(coverImageView);
+        isbnTextView.setText("ISBN : " + book.isbn);
+        priceTextView.setText(String.valueOf(book.price) + " €");
+
+        final String uri = AMAZON_ENDPOINT + book.title;
+
+
+        buyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.addCategory(Intent.CATEGORY_BROWSABLE);
+                intent.setData(Uri.parse(uri));
+                startActivity(intent);
+            }
+        });
     }
 }
